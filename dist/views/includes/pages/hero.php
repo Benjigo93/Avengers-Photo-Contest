944925e774e9a575f41c281452e59a3aac81e15c
Unknown <jn.duchevet@gmail.com>
2018-04-12 13:17:36 +0200

    Fix 3.5
    Fix pages characters-list and hero

--- dist/views/includes/pages/hero.php
+++ dist/views/includes/pages/hero.php
@@ -5,7 +5,7 @@ if(isset($_GET['shId'])) {
     $apiKey = '1741355745903622';
     $apiUrl = 'http://www.superheroapi.com/api/'.$apiKey.'/'.$id;
     
-    $path = '../../cache/superheroes'.md5($apiUrl);
+    $path = '../../cache/superheroes/'.md5($apiUrl);
 
     if(file_exists($path) && time() - filemtime($path) < 86400)
     {
@@ -19,7 +19,7 @@ if(isset($_GET['shId'])) {
 }
 
     // echo '<pre>';
-    // print_r($heroData);
+    // var_dump($heroData);
     // echo '</pre>';
 
 ?>
@@ -31,31 +31,30 @@ if(isset($_GET['shId'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../../styles/main.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
     <title><?= $heroData->name; ?></title>
 </head>
 <body>
     <div id="desc-hero">
-        <a href="characters-list.php" class="back-button">All heroes</a>
-        <h1><?= $heroData->name; ?></h1>
+        <a href="characters-list.php" class="back-button"><i class="fas fa-angle-left"></i>All heroes</a>
+        <div class="title">
+            <h1><?= $heroData->name; ?></h1>
+            <div class="line"></div>
+        </div>
         <div class="part-1">
             <img src="../../../assets/images/sheroe<?= $id ?>.jpg" class="picture">
             <div class="infos">
-                <div>FULL NAME : <?= $heroData->biography->{'full-name'}; ?></div>
-                <div>RACE : <?= $heroData->appearance->race; ?></div>
-                <div>HEIGHT : <?= $heroData->appearance->height[1]; ?> /  <?= $heroData->appearance->height[0]; ?></div>
-                <div>WEIGHT : <?= $heroData->appearance->weight[1]; ?> /  <?= $heroData->appearance->weight[0]; ?></div>
-                <div>AFFILIATIONS (COMICS): <?= $heroData->connections->{'group-affiliation'}; ?></div>
+                <div><span class="infos-type">FULL NAME :</span> <?= $heroData->biography->{'full-name'}; ?></div>
+                <div><span class="infos-type">RACE :</span> <?= $heroData->appearance->race; ?></div>
+                <div><span class="infos-type">HEIGHT :</span> <?= $heroData->appearance->height[1]; ?> /  <?= $heroData->appearance->height[0]; ?></div>
+                <div><span class="infos-type">WEIGHT :</span> <?= $heroData->appearance->weight[1]; ?> /  <?= $heroData->appearance->weight[0]; ?></div>
+                <div><span class="infos-type">AFFILIATIONS (COMICS):</span> <?= $heroData->connections->{'group-affiliation'}; ?></div>
             </div>
         </div>
         <div class="part-2">
-            <h2>Characteristics</h2>
-            <div>INTELLIGENCE: <?= $heroData->powerstats->intelligence; ?> %</div>
-            <div>STRENGTH: <?= $heroData->powerstats->strength; ?> %</div>
-            <div>SPEED: <?= $heroData->powerstats->speed; ?> %</div>
-            <div>DURABILITY: <?= $heroData->powerstats->durability; ?> %</div>
-            <div>POWER: <?= $heroData->powerstats->power; ?> %</div>
-            <div>COMBAT: <?= $heroData->powerstats->combat; ?> %</div>
         </div>
     </div>
+
+    <script src="../../../scripts/hero.js"></script>
 </body>
 </html>
